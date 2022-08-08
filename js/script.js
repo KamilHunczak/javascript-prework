d267d8dd5497c49e207b9282b6dbbbc7f10ed36c
Kamil Huńczak <hunczak.kamil@gmail.com>
2022-08-08 22:01:43 +0200

    add computer move

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
+let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-let computerMove = 'kamień';
-let playerMove = 'papier'
+console.log('Wylosowana liczba to: ' + randomNumber);
 
-printMessage('Zagrałem ' + computerMove +'! Jeśli Twój ruch to '+ playerMove +', to wygrywasz!');
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1){
+  computerMove = 'kamień';
+}
+else if(randomNumber == 2){
+    computerMove = 'papier';
+}
+else if(randomNumber == 3) {
+    computerMove = 'nożyce';
+}
+
+printMessage('Mój ruch to: ' + computerMove);
