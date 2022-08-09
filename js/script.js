5970d5d673e24b918561dc3959f8f706c64a2406
Kamil Huńczak <hunczak.kamil@gmail.com>
2022-08-09 22:52:14 +0200

    add buttons to the game

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-//functions
 function getMoveName(MoveId){
   if(MoveId == 1){
     return 'kamień';
@@ -12,15 +11,11 @@ function getMoveName(MoveId){
   printMessage('Nie znam ruchu o id ' + MoveId + '.');
   return 'nieznany ruch';
 }
-  
+
 function displayResult(computerMove, playerMove){
-  if( computerMove == 'kamień' && playerMove == 'papier'){
-    return 'Ty wygrywasz!';
-  }
-  else if(computerMove == 'papier' && playerMove == 'nożyce'){
-    return 'Ty wygrywasz!';
-  }
-  else if(computerMove == 'nożyce' && playerMove == 'kamień'){
+  if( computerMove == 'kamień' && playerMove == 'papier'||
+      computerMove == 'papier' && playerMove == 'nożyce'||
+      computerMove == 'nożyce' && playerMove == 'kamień'){
     return 'Ty wygrywasz!';
   }
   else if(computerMove == playerMove) {
@@ -31,26 +26,32 @@ function displayResult(computerMove, playerMove){
   }
   }
 
-//computer move
-let randomNumber = Math.floor(Math.random() * 3 + 1);
+function playGame(playerInput){
+  clearMessages();
 
-console.log('Wylosowana liczba to: ' + randomNumber);
+  //computer move
+  let randomNumber = Math.floor(Math.random() * 3 + 1);
+  let computerMove = getMoveName(randomNumber);
+  printMessage('Mój ruch to: ' + computerMove);
 
-let computerMove = getMoveName(randomNumber);
+  //player move
+  let playerMove = getMoveName(playerInput);
+  printMessage('Twój ruch to: ' + playerMove);
 
-printMessage('Mój ruch to: ' + computerMove);
-
-
-//player move
-
-let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-
-console.log('Gracz wpisał: ' + playerInput);
-
-let playerMove = getMoveName(playerInput);
-
-printMessage('Twój ruch to: ' + playerMove);
+  printMessage(displayResult(computerMove, playerMove));
+}
 
 
-//game score
-printMessage(displayResult(computerMove, playerMove));
+document.getElementById('play-rock').addEventListener('click', function(){
+  playGame(1);
+});
+document.getElementById('play-paper').addEventListener('click', function(){
+  playGame(2);
+});
+document.getElementById('play-scissors').addEventListener('click', function(){
+  playGame(3);
+});
+
+// document.getElementById('play-rock').addEventListener('click', playGame(1));
+
+
