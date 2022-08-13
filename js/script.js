8bd6eae490fb0b500b9a1ddded531180084b1345
Kamil Huńczak <hunczak.kamil@gmail.com>
2022-08-13 15:34:10 +0200

    change let to const, change named functions to anonymous functions & move them to smaller scopes

--- js/script.js
+++ js/script.js
@@ -1,56 +1,54 @@
-function getMoveName(MoveId){
-  if(MoveId == 1){
-    return 'rock';
-  }
-  else if(MoveId == 2){
-    return 'paper';
-  }
-  else if(MoveId == 3){
-    return 'scisors';
-  }
-}
+{
+const playGame= (playerInput)=>{
 
-function displayResult(computerMove, playerMove){
-  if( computerMove == 'rock' && playerMove == 'paper'||
-      computerMove == 'paper' && playerMove == 'scisors'||
-      computerMove == 'scisors' && playerMove == 'rock'){
-    return 'You Win!';
+  const getMoveName = (MoveId) => {
+    if(MoveId == 1){
+      return 'rock';
+    }
+    else if(MoveId == 2){
+      return 'paper';
+    }
+    else if(MoveId == 3){
+      return 'scisors';
+    }
   }
-  else if(computerMove == playerMove) {
-    return 'Draw';
-  }
-  else{
-    return 'You Lost';
-  }
-  }
-
-function playGame(playerInput){
+  
   clearMessages();
   
-  
   //computer move
-  let randomNumber = Math.floor(Math.random() * 3 + 1);
-  let computerMove = getMoveName(randomNumber);
+  const randomNumber = Math.floor(Math.random() * 3 + 1);
+  const computerMove = getMoveName(randomNumber);
   document.getElementById('computer-choice-image').src='images/'+computerMove+'.png';
 
-
   //player move
-  let playerMove = getMoveName(playerInput);
+  const playerMove = getMoveName(playerInput);
   document.getElementById('player-choice-image').src='images/'+playerMove+'.png';
 
+
+  const displayResult = (computerMove, playerMove) => {
+    if( computerMove == 'rock' && playerMove == 'paper'||
+        computerMove == 'paper' && playerMove == 'scisors'||
+        computerMove == 'scisors' && playerMove == 'rock'){
+      return 'You Win!';
+    }
+    else if(computerMove == playerMove){
+      return 'Draw';
+    }
+    else{
+      return 'You Lost';
+    }
+  }
   printMessage(displayResult(computerMove, playerMove));
   
 }
 
-
-document.getElementById('play-rock').addEventListener('click', function(){
-  playGame(1);
-});
-document.getElementById('play-paper').addEventListener('click', function(){
-  playGame(2);
-});
-document.getElementById('play-scissors').addEventListener('click', function(){
-  playGame(3);
-});
-
-
+  document.getElementById('play-rock').addEventListener('click', ()=> {
+    playGame(1);
+  });
+  document.getElementById('play-paper').addEventListener('click', ()=> {
+    playGame(2);
+  });
+  document.getElementById('play-scissors').addEventListener('click', ()=> {
+    playGame(3);
+  });
+}
